7bbd9acb8cf809dc1830c8a3f5c3f793701a79a8
RobTheThief <cyberpeaches@gmail.com>
2022-03-13 17:44:20 +0000

    obfuscate token

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,7 +38,7 @@ function getRandomWord () {
                 method: "GET",
                 headers: {
                     "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-                    "x-rapidapi-key": "51ab0f2606mshb2d552426462fe4p136abfjsne76bf417bf32"
+                    "x-rapidapi-key": token,
                 }
             });   
     
