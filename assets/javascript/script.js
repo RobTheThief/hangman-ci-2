dcaea964b4b633e4b3c616e3bdc8312e237df329
RobTheThief <cyberpeaches@gmail.com>
2022-03-14 00:30:05 +0000

    clear text content of modal on close with x box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,14 +12,15 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    document.getElementById('modal-text').textContent = '';
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
     document.getElementById('modal-text').textContent = '';
+    modal.style.display = "none";
   }
 }
 /* ******************************************************************* */
